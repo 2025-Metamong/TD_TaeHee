99b4986e6135ad61202e24ba275d2c9e32d52168
taehee <gmglgmgn@naver.com>
2025-05-13 14:58:57 +0900

    faet:Add TowerID in TowerManager and Tower Scripts 2

--- Assets/Scripts/Objects/Tower.cs
+++ Assets/Scripts/Objects/Tower.cs
@@ -10,7 +10,7 @@ namespace MyGame.Objects
     {
         // Represents a tower that also serves as a bullet generator.
         [Header("Tower Stats")]   // 타워 정보 inspector에 표기하기 위한 용도.
-        [SerializeField, Tooltip("타워 ID")] private int ID = -1;
+        [SerializeField, Tooltip("타워 ID")] private int ID = -1;   // 디폴트는 -1로 설정.
         [SerializeField, Tooltip("설치 비용")] private int cost = 10;
         [SerializeField, Tooltip("설치 위치")] private Transform position;
         [SerializeField, Tooltip("사거리")] private float range = 10f;
