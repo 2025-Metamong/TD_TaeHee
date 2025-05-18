8af9288a6c26a4b3a6f13686bc5df7d8f30b8f6a
taehee <gmglgmgn@naver.com>
2025-05-19 00:05:29 +0900

    refactor:TowerPlacementTile.cs 에 주석 추가

--- Assets/Scripts/Objects/TowerPlacementTile.cs
+++ Assets/Scripts/Objects/TowerPlacementTile.cs
@@ -14,14 +14,18 @@ public class TowerPlacementTile : MonoBehaviour
     private Renderer rend;  // 타일의 랜더러 컴포넌트 저장용.
     private Color originalColor;
     private MaterialPropertyBlock propBlock;
-    private static readonly int _BaseColorID = Shader.PropertyToID("_BaseColor");
     // URP Lit 머티리얼의 Base Color 프로퍼티 이름
+    private static readonly int _BaseColorID = Shader.PropertyToID("_BaseColor");
+    private static readonly Vector3 offset = new Vector3(0, 4.5f, 0);   // 타일의 높이는 4.5
 
     void Awake()
     {
+        // MaterialPropertyBlock 을 이용해서 Renderer에 접근하면 훨씬 빠르다고 함.
         rend = GetComponent<Renderer>();
         propBlock = new MaterialPropertyBlock();
+        // 원래 타일 배경색 저장해두기.
         originalColor = rend.sharedMaterial.GetColor(_BaseColorID);
+        // 타일 위치 저장해두기.
         this.tilePosition = this.transform.position;
     }
 
@@ -29,18 +33,18 @@ public class TowerPlacementTile : MonoBehaviour
     void OnMouseEnter()
     {
         Debug.Log("타일에 마우스 들어옴");
-        rend.GetPropertyBlock(propBlock);
-        propBlock.SetColor(_BaseColorID, hoverColor);
-        rend.SetPropertyBlock(propBlock);
+        rend.GetPropertyBlock(propBlock);   // MaterialPropertyBlock에서 값 복사해오기
+        propBlock.SetColor(_BaseColorID, hoverColor);   // 타일 색상 변경
+        rend.SetPropertyBlock(propBlock);   // MaterialPropertyBlock에 변경 값 저장하기
     }
 
     // 마우스가 타일에서 나갈 때 원래 색상으로 복귀
     void OnMouseExit()
     {
         Debug.Log("타일에서 마우스 나감");
-        rend.GetPropertyBlock(propBlock);
-        propBlock.SetColor(_BaseColorID, originalColor);
-        rend.SetPropertyBlock(propBlock);
+        rend.GetPropertyBlock(propBlock);   // MaterialPropertyBlock에서 값 복사해오기
+        propBlock.SetColor(_BaseColorID, originalColor);    // 타일 색상 되돌리기
+        rend.SetPropertyBlock(propBlock);   // MaterialPropertyBlock에 변경 값 저장하기
     }
 
     // 해당 타일을 클릭했을 때 타워 생성 호출.
@@ -52,7 +56,7 @@ public class TowerPlacementTile : MonoBehaviour
             return;
         }
         // 현재 타일 위치 타워 매니저에 전달해서 타워 설치 요청.
-        Vector3 installPosition = new Vector3(tilePosition.x, tilePosition.y+4.5f, tilePosition.z);
+        Vector3 installPosition = this.tilePosition + offset;
         TowerManager.Instance.InstallTower(installPosition);
     }
 }
