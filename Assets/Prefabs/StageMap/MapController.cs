154d1bdfe6599d8de8430f00e97d4f5392a14cd2
freedomera <freedomera627@gmail.com>
2025-05-25 03:56:46 +0900

    feat: (Camera) block 지정 카메라 범위 제한 기능 추가

--- Assets/Prefabs/StageMap/MapController.cs
+++ Assets/Prefabs/StageMap/MapController.cs
@@ -4,6 +4,23 @@ public class MapController : MonoBehaviour
 {
     [Tooltip("몬스터가 스폰될 위치")]
     public Transform spawnPoint;
+
     [Tooltip("웨이 포인트가 담긴 부모 오브젝트")]
     public Transform pathHolder;
+
+    [Tooltip("카메라 이동 한계 블럭 1")]
+    public GameObject block1;
+    [ReadOnly]
+    public Vector3 pos1;
+
+    [Tooltip("카메라 이동 한계 블럭 2")]
+    public GameObject block2;
+    [ReadOnly]
+    public Vector3 pos2;
+
+    private void OnValidate()
+    {
+        if (block1 != null) pos1 = block1.transform.position;
+        if (block2 != null) pos2 = block2.transform.position;
+    }
 }
