913f9d1e8328e64e4e67f0b7b3ee99ee11c9be04
swoon427 <swoon427@gmail.com>
2025-05-25 17:14:40 +0900

    stage Clear UI add

---
+++ Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,35 @@
+using MyGame.Managers;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyGame.UI
+{
+    public class RestartButton : MonoBehaviour
+    {
+        public static RestartButton Instance { get; private set; }
+
+        private Button _button;
+        int currentStage;
+        void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnClicked);
+        }
+
+        void OnEnable()
+        {
+            currentStage = StageManager.Instance.getCurrentStage;
+        }
+
+        private void OnClicked()
+        {
+            StageManager.Instance.ExitStage();
+            GameManager.Instance.ExitScene();
+            GameManager.Instance.LoadScene("InStage");
+            StageManager.Instance.LoadStage(currentStage);
+        }
+
+
+    }
+}
